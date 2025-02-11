bbe3a4e7c789a11c1e81166621cff5d9d1926166
Monu Yadav <154664881+Monuyadav18@users.noreply.github.com>
2025-02-11 23:20:20 +0530

    Add files via upload

---
+++ Data Types/Lecture/src/Escape.java
@@ -0,0 +1,12 @@
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("Hello Coders!");
+        System.out.println("Hello \tCoders!");
+        System.out.println("Hello \bCoders!");
+        System.out.println("Hello \nCoders!");
+        System.out.println("Hello 'Coders!'");
+        System.out.println("Hello \"Coders!\"");
+        System.out.println("Hello \\Coders!\\");
+
+    }
+}
---
+++ Data Types/Lecture/src/Variables.java
@@ -0,0 +1,22 @@
+import java.sql.SQLOutput;
+
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber = 865;
+        int YourNumber;
+        YourNumber = 888;
+        System.out.println(YourNumber);
+
+        YourNumber = 7;
+        System.out.println(YourNumber);
+
+        float MyFloat = 5;
+        System.out.println(MyFloat);
+
+        boolean IsVegetarian = false;
+        System.out.println(IsVegetarian);
+
+        String wishes = "Good Morning";
+        System.out.println(wishes);
+    }
+}
